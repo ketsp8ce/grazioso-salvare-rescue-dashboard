5aa3ecc519c1843e39c172263847966127e16f7e
ketsp8ce <hunter.marx@snhu.edu>
2025-10-15 07:08:24 -0500

    Added code for CRUD functionality to class pokemonCRUD

--- pokemonCRUD.py
+++ pokemonCRUD.py
@@ -1,16 +1,27 @@
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 
 class PokemonCRUD:
-    """CRUD operations for Kanto Pokémon collection in MongoDB"""
+    """Minimal CRUD for Kanto Pokémon in MongoDB"""
 
-    # default constructor
+    def __init__(self, uri="mongodb://localhost:27017/", db_name="pokemon_dashboard", col_name="kanto_pokemon"):
+        self.client = MongoClient(uri)
+        self.collection = self.client[db_name][col_name]
 
-    # Create
+    def create(self, data):
+        if data:
+            return self.collection.insert_one(data).acknowledged
+        return False
 
-    # Read
+    def read(self, query=None): 
+        query = query or {}
+        return list(self.collection.find(query))
 
-    # Update
+    def update(self, query, update_data):
+        if query and update_data:
+            return self.collection.update_many(query, {"$set": update_data}).modified_count
+        return 0
 
-    # Delete
-
+    def delete(self, query):
+        if query:
+            return self.collection.delete_many(query).deleted_count
+        return 0
