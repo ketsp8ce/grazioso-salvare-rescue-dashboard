b22179e3ed95dba07806164da8bf78e3abb3f767
ketsp8ce <hunter.marx@snhu.edu>
2025-10-15 07:20:04 -0500

    Updated app.py to reference CRUD methods

--- app.py
+++ app.py
@@ -1,40 +1,37 @@
 from flask import Flask, jsonify
-from pymongo import MongoClient
+from PokemonCRUD import PokemonCRUD  # make sure the class name matches your file
 
 app = Flask(__name__)
 
-# Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/")
-db = client['pokemon_dashboard']
-collection = db['kanto_pokemon']
-
-# Example: print one document
-print(collection.find_one())
+# Instantiate your CRUD class
+crud = PokemonCRUD()
 
 # route to call home
 @app.route('/')
 def home():
-    # Example: return the first document in the collection
-    first_doc = collection.find_one()
-    if first_doc:
+    # Get the first Pokémon document
+    docs = crud.read({})
+    if docs:
+        first_doc = docs[0]
         first_doc['_id'] = str(first_doc['_id'])  # Convert ObjectId to string
         return jsonify(first_doc)
     else:
         return jsonify({"message": "No data found"})
 
-# route to return all pokemon
+# route to return all Pokémon
 @app.route('/all')
 def all_pokemon():
-    docs = list(collection.find())
+    docs = crud.read({})
     for doc in docs:
         doc['_id'] = str(doc['_id'])
     return jsonify(docs)
 
-# route to return a single pokemon
+# route to return a single Pokémon by ID
 @app.route('/pokemon/<int:pokemon_id>')
 def get_pokemon(pokemon_id):
-    doc = collection.find_one({"id": pokemon_id})
-    if doc:
+    docs = crud.read({"id": pokemon_id})
+    if docs:
+        doc = docs[0]
         doc['_id'] = str(doc['_id'])
         return jsonify(doc)
     else:
